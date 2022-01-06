3e51194098c730f0375e03b26a503354dc189ad0
이상철 <zlrz6706@gmail.com>
2022-01-06 10:48:16 +0900

    2022-01-06

---
+++ 백준 10818.js
@@ -0,0 +1,17 @@
+// 최소 최대 값 구하는 문제
+
+const a = "5\n1 2 3 4 5";
+
+const v = a.split("\n");
+
+console.log(v);
+
+const numbers = v[1].split(" ").map(Number)
+
+console.log(numbers);
+
+
+console.log(Math.max(...numbers) , Math.min(...numbers))
+
+
+console.log(numbers)
